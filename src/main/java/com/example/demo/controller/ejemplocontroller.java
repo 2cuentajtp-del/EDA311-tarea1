b5a0ae2cb6e9c799407540b013c992d573e4a045
jhonatantp <2cuentajtp@gmail.com>
2026-08-26 09:53:00 -0400

    actualizado por el avance de la clase 26/08
    avance

--- src/main/java/com/example/demo/controller/ejemplocontroller.java
+++ src/main/java/com/example/demo/controller/ejemplocontroller.java
@@ -1,13 +1,42 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ejemplocontroller {
+@RequestMapping("/api/v1")
+public class EjemploController {
+
+    int[] edades = new int[7];
 
     @GetMapping("/")
     public String hola() {
         return "hola hija";
+    }
+
+    @GetMapping("/edades")
+    public int[] getEdades() {
+        edades[0] = 19;
+        edades[1] = 18;
+        edades[2] = 17;
+        edades[3] = 16;
+        edades[4] = 15;
+        edades[5] = 14;
+        edades[6] = 13;
+        return edades;
+    }
+
+    @GetMapping("/agregarEdad/{edadNueva}")
+    public int[] agregarEdad(@PathVariable int edadNueva) {
+        int[] edadesExtendido = new int[edades.length + 1];
+
+        for (int i = 0; i < edades.length; i++) {
+            edadesExtendido[i] = edades[i];
+        }
+
+        edadesExtendido[edades.length] = edadNueva;
+        return edadesExtendido;
     }
 }
